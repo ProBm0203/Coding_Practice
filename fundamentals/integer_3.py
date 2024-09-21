0ed65e071e14614cf2a6fab5c8f88414b773cc6f
Bhaskar Mishra <bhaskarmishra12675@gmail.com>
2024-09-21 14:27:17 +0530

    Optimizing Code

--- fundamentals/integer_3.py
+++
@@ -1,8 +0,0 @@
-'''Write a program to take 2-digit number and then print the reversed number. 
-For example, if the input given is 25, the program should print 52.'''
-
-print("---------------------------------\n     Author : Bhaskar Mishra     \n---------------------------------")
-num=int(input("Enter a 2-digit number : "))
-print("Reversed number is : ",end="")
-print(num%10,num//10,sep="")
-print()
--- fundamentals/integer_4.py
+++ fundamentals/integer_4.py
@@ -1,12 +1,9 @@
-'''Write a program to take 3-digit number and then print the reversed number. 
+'''Write a program to take number and then print the reversed number. 
 For example, if the input given is 123, the program should print 321.'''
 
 print("---------------------------------\n     Author : Bhaskar Mishra     \n---------------------------------")
-num=int(input("Enter a 3-digit number : "))
-print("Reversed number is : ",end="")
-print((num%100)%10,(num%100)//10,num//100,sep="")
-print("OR")  #There is an another logic shown written below
-num2=str(num)
-num2=num2[-1::-1]
-print(int(num2))
+num=int(input("Enter a number : "))
+num=str(num)
+num=num[::-1]
+print(int(num))
 print()
